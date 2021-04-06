0c61663db0ff5ff1e4745e35759dc790d2d0242e
Olal Jnr <42208250+Molal@users.noreply.github.com>
2021-04-06 22:56:48 +0300

    Add files via upload

---
+++ Assignment 2 .py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Name = input("Name:")
+country = input("Country:")
+currentjob= input("Profession:")
+email = input("Email Address:")
+phone= input( "Phone Number:")
+city = input("City:")
+address = input("Postal Address:")
+
+
+# In[ ]:
+
+
+
+
